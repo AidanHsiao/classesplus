5c5a8e1099eb75bb70774dccc7d6609aa3b86082
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2020-04-02 01:51:12 -0700

    major website update
    everything i missed previously

---
+++ script.js
@@ -0,0 +1,105 @@
+var s111 = 0;
+var s110 = 0;
+var s104 = 0;
+var s103 = 0;
+var s102 = 0;
+var s101 = 0;
+var s100 = 0;
+
+function extend111() {
+	if (s111 == 0) {
+	document.getElementById("111").style.maxHeight = "60px";
+	document.getElementById("111").style.padding = "20px";
+	s111++;
+	document.getElementById("symbol111").innerHTML = "-";
+	} else {
+	document.getElementById("111").style.maxHeight = "0px";
+	document.getElementById("111").style.padding = "0px 20px";
+	s111 = 0;
+	document.getElementById("symbol111").innerHTML = "+";
+	}
+}
+
+function extend110() {
+	if (s110 == 0) {
+	document.getElementById("110").style.maxHeight = "330px";
+	document.getElementById("110").style.padding = "20px";
+	s110++;
+	document.getElementById("symbol110").innerHTML = "-";
+	} else {
+	document.getElementById("110").style.maxHeight = "0px";
+	document.getElementById("110").style.padding = "0px 20px";
+	s110 = 0;
+	document.getElementById("symbol110").innerHTML = "+";
+	}
+}
+
+function extend104() {
+	if (s104 == 0) {
+	document.getElementById("104").style.maxHeight = "420px";
+	document.getElementById("104").style.padding = "20px";
+	s104++;
+	document.getElementById("symbol104").innerHTML = "-";
+	} else {
+	document.getElementById("104").style.maxHeight = "0px";
+	document.getElementById("104").style.padding = "0px 20px";
+	s104 = 0;
+	document.getElementById("symbol104").innerHTML = "+";
+	}
+}
+
+function extend103() {
+	if (s103 == 0) {
+	document.getElementById("103").style.maxHeight = "270px";
+	document.getElementById("103").style.padding = "20px";
+	s103++;
+	document.getElementById("symbol103").innerHTML = "-";
+	} else {
+	document.getElementById("103").style.maxHeight = "0px";
+	document.getElementById("103").style.padding = "0px 20px";
+	s103 = 0;
+	document.getElementById("symbol103").innerHTML = "+";
+	}
+}
+
+function extend102() {
+	if (s102 == 0) {
+	document.getElementById("102").style.maxHeight = "210px";
+	document.getElementById("102").style.padding = "20px";
+	s102++;
+	document.getElementById("symbol102").innerHTML = "-";
+	} else {
+	document.getElementById("102").style.maxHeight = "0px";
+	document.getElementById("102").style.padding = "0px 20px";
+	s102 = 0;
+	document.getElementById("symbol102").innerHTML = "+";
+	}
+}
+
+function extend101() {
+	if (s101 == 0) {
+	document.getElementById("101").style.maxHeight = "240px";
+	document.getElementById("101").style.padding = "20px";
+	s101++;
+	document.getElementById("symbol101").innerHTML = "-";
+	} else {
+	document.getElementById("101").style.maxHeight = "0px";
+	document.getElementById("101").style.padding = "0px 20px";
+	s101 = 0;
+	document.getElementById("symbol101").innerHTML = "+";
+	}
+}
+
+function extend100() {
+	if (s100 == 0) {
+	document.getElementById("100").style.maxHeight = "210px";
+	document.getElementById("100").style.padding = "20px";
+	s100++;
+	document.getElementById("symbol100").innerHTML = "-";
+	} else {
+	document.getElementById("100").style.maxHeight = "0px";
+	document.getElementById("100").style.padding = "0px 20px";
+	s100 = 0;
+	document.getElementById("symbol100").innerHTML = "+";
+	}
+}
